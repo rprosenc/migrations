cad203f1f1f8e291d09fea3a8a8e69581b1f8206
Martin Prebio <mp@25th-floor.com>
2012-09-21 19:36:34 +0200

    Refactoring needed for filesystem mocking

--- lib/TwentyFifth/Migrations/Manager/FileManager.php
+++ lib/TwentyFifth/Migrations/Manager/FileManager.php
@@ -20,7 +20,7 @@ class FileManager
 
 	public function getOrderedFileList()
 	{
-		$directory_content = glob($this->directory . '*');
+		$directory_content = $this->readDir();
 
 		foreach ($directory_content as &$file) {
 			$file = basename($file);
@@ -32,9 +32,29 @@ class FileManager
 		$files = array();
 
 		foreach ($directory_content as $file) {
-			$files[$file] = $this->directory . $file;
+			$files[$file] = $this->directory . '/' . $file;
 		}
 
+		return $files;
+	}
+
+	protected function readDir()
+	{
+		$dir_handle = opendir($this->directory);
+
+		$files = array();
+		while(($file = readdir($dir_handle)) !== false) {
+			$files[] = $file;
+		}
+
+		// Filter non sql files
+		foreach ($files as $index => $file) {
+			if (!preg_match('@.sql$@i', $file)) {
+				unset ($files[$index]);
+			}
+		}
+
+		closedir($dir_handle);
 		return $files;
 	}
 }

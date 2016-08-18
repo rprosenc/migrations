7603ea09359f370313b1b69e3516aba81d157420
Thomas Subera <ts@25th-floor.com>
2016-08-18 11:31:40 +0200

    introduce new option --user to allow overriding the username
    also changed tabs to spaces according to 25th code standards. (sorry mate)

--- bin/migrations.php
+++ bin/migrations.php
@@ -6,50 +6,50 @@ use TwentyFifth\Migrations\Command;
 
 function includeIfExists($file)
 {
-	if (file_exists($file)) {
-		return include $file;
-	}
+    if (file_exists($file)) {
+        return include $file;
+    }
 }
 
-if ((!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
-	die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
-		'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-		'php composer.phar install'.PHP_EOL);
+if ((!$loader = includeIfExists(__DIR__ . '/../../../autoload.php'))) {
+    die('You must set up the project dependencies, run the following commands:' . PHP_EOL .
+        'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL);
 }
 
 $sqlDirectory = '';
 
 // ZF2
 if (is_readable(__DIR__ . '/../config/application.config.php')) {
-	chdir(dirname(__DIR__));
-	$configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\ZF2Manager(require __DIR__ . '/../config/application.config.php');
+    chdir(dirname(__DIR__));
+    $configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\ZF2Manager(require __DIR__ . '/../config/application.config.php');
 
-	$sqlDirectory = __DIR__ . '/../docs/sql/';
+    $sqlDirectory = __DIR__ . '/../docs/sql/';
 } else if (is_readable(__DIR__ . '/../../../../config/application.config.php')) {
-	chdir(dirname(__DIR__) . '/../../../');
-	$configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\ZF2Manager(require __DIR__ . '/../../../../config/application.config.php');
+    chdir(dirname(__DIR__) . '/../../../');
+    $configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\ZF2Manager(require __DIR__ . '/../../../../config/application.config.php');
 
-	$sqlDirectory = __DIR__ . '/../../../../docs/sql/';
+    $sqlDirectory = __DIR__ . '/../../../../docs/sql/';
 
 // ZF1
 } else if (is_dir(__DIR__ . '/../application/')) {
-	$configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\ZF1Manager(__DIR__ . '/../application/');
-	$sqlDirectory = __DIR__ . '/../docs/sql/';
+    $configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\ZF1Manager(__DIR__ . '/../application/');
+    $sqlDirectory = __DIR__ . '/../docs/sql/';
 } else if (is_dir(__DIR__ . '/../../../../application/')) {
-	$configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\ZF1Manager(__DIR__ . '/../../../../application/');
-	$sqlDirectory = __DIR__ . '/../../../../docs/sql/';
+    $configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\ZF1Manager(__DIR__ . '/../../../../application/');
+    $sqlDirectory = __DIR__ . '/../../../../docs/sql/';
 
 // Symfony
 } else if (is_readable(__DIR__ . '/../app/config/parameters.yml')) {
-	$configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\SymfonyManager(__DIR__ . '/../app/config/parameters.yml');
-	$sqlDirectory = __DIR__ . '/../sql/';
+    $configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\SymfonyManager(__DIR__ . '/../app/config/parameters.yml');
+    $sqlDirectory = __DIR__ . '/../sql/';
 } else if (is_readable(__DIR__ . '/../../../../app/config/parameters.yml')) {
-	$configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\SymfonyManager(__DIR__ . '/../../../../app/config/parameters.yml');
-	$sqlDirectory = __DIR__ . '/../../../../sql/';
+    $configManager = new \TwentyFifth\Migrations\Manager\ConfigManager\SymfonyManager(__DIR__ . '/../../../../app/config/parameters.yml');
+    $sqlDirectory = __DIR__ . '/../../../../sql/';
 
 // UNKNOWN
 } else {
-	die('Could not set APPLICATION_PATH'.PHP_EOL);
+    die('Could not set APPLICATION_PATH' . PHP_EOL);
 }
 
 $fileManager = new \TwentyFifth\Migrations\Manager\FileManager($sqlDirectory);
--- lib/TwentyFifth/Migrations/Manager/ConfigManager/ZF1Manager.php
+++ lib/TwentyFifth/Migrations/Manager/ConfigManager/ZF1Manager.php
@@ -10,93 +10,100 @@
 namespace TwentyFifth\Migrations\Manager\ConfigManager;
 
 
-class ZF1Manager implements ConfigInterface {
+class ZF1Manager implements ConfigInterface
+{
 
-	private $application_path;
+    private $application_path;
 
-	/**
-	 * @var array
-	 */
-	private $config;
+    /**
+     * @var array
+     */
+    private $config;
 
-	/**
-	 * @param $application_path
-	 */
-	function __construct($application_path)
-	{
-		if (!getenv('APPLICATION_ENV')) {
-			die('Please set the APPLICATION_ENV'.PHP_EOL);
-		}
+    /**
+     * @param $application_path
+     */
+    function __construct($application_path)
+    {
+        if (!getenv('APPLICATION_ENV')) {
+            die('Please set the APPLICATION_ENV' . PHP_EOL);
+        }
 
-		$this->application_path = $application_path;
+        $this->application_path = $application_path;
 
-		$zend_config = new \Zend_Config_Ini($application_path . '/configs/application.ini', getenv('APPLICATION_ENV'));
-		$this->setConfig($zend_config);
-	}
+        $zend_config = new \Zend_Config_Ini($application_path . '/configs/application.ini', getenv('APPLICATION_ENV'));
+        $this->setConfig($zend_config);
+    }
 
-	/**
-	 * initialize config array
-	 *
-	 * @param \Zend_Config_Ini $zend_config
-	 *
-	 * @return $this
-	 */
-	protected function setConfig(\Zend_Config_Ini $zend_config)
-	{
-		$config = $zend_config->resources->doctrine->toArray();
-		$defaultConnection = $config['dbal']['defaultConnection'];
-		$this->config = $config['dbal']['connections'][$defaultConnection]['parameters'];
-		return $this;
-	}
+    /**
+     * initialize config array
+     *
+     * @param \Zend_Config_Ini $zend_config
+     *
+     * @return $this
+     */
+    protected function setConfig(\Zend_Config_Ini $zend_config)
+    {
+        $config = $zend_config->resources->doctrine->toArray();
+        $defaultConnection = $config['dbal']['defaultConnection'];
+        $this->config = $config['dbal']['connections'][$defaultConnection]['parameters'];
+        return $this;
+    }
 
-	/**
-	 * @return string Hostname
-	 */
-	public function getHost()
-	{
-		return $this->config['host'];
-	}
+    /**
+     * @return string Hostname
+     */
+    public function getHost()
+    {
+        return $this->config['host'];
+    }
 
-	/**
-	 * @return string Port
-	 */
-	public function getPort()
-	{
-		return $this->config['port'];
-	}
+    /**
+     * @return string Port
+     */
+    public function getPort()
+    {
+        return $this->config['port'];
+    }
 
-	/**
-	 * @return string Database name
-	 */
-	public function getDatabase()
-	{
-		return $this->config['dbname'];
-	}
+    /**
+     * @return string Database name
+     */
+    public function getDatabase()
+    {
+        return $this->config['dbname'];
+    }
 
-	/**
-	 * @param string $database
-	 *
-	 * @return ConfigInterface
-	 */
-	public function setDatabase($database)
-	{
-		$this->config['dbname'] = $database;
-		return $this;
-	}
+    /**
+     * @param string $database
+     *
+     * @return ConfigInterface
+     */
+    public function setDatabase($database)
+    {
+        $this->config['dbname'] = (string)$database;
+        return $this;
+    }
 
-	/**
-	 * @return string Username
-	 */
-	public function getUsername()
-	{
-		return $this->config['user'];
-	}
+    /**
+     * @return string Username
+     */
+    public function getUsername()
+    {
+        return $this->config['user'];
+    }
 
-	/**
-	 * @return string Password
-	 */
-	public function getPassword()
-	{
-		return $this->config['password'];
-	}
+    public function setUserName($username)
+    {
+        $this->config['user'] = (string)$username;
+        return $this;
+    }
+
+    /**
+     * @return string Password
+     */
+    public function getPassword()
+    {
+        return $this->config['password'];
+    }
 }
